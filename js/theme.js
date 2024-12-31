d3cc5b81ac70f58bf2a0e494a3ef9aba0a0acc9d
MrTastyDuck <168537601+MrTastyDuck@users.noreply.github.com>
2024-12-31 18:28:49 +0100

    changecss final 1h

--- js/theme.js
+++ js/theme.js
@@ -1,17 +1,13 @@
 function toggleCSSTheme() {
     const currentCSSTheme = localStorage.getItem('csstheme') || 'lightmode';
     const newCSSTheme = currentCSSTheme === 'lightmode' ? 'darkmode' : 'lightmode';
-    
-    // Set the base URL for your CSS files
-    const baseURL = '../css/'; // Adjust this if necessary to point to the correct folder
+    const baseURL = '/css/';
     document.getElementById('csstheme').setAttribute('href', `${baseURL}${newCSSTheme}.css`);
     localStorage.setItem('csstheme', newCSSTheme);
 }
 
 window.onload = function() {
     const csstheme = localStorage.getItem('csstheme') || 'lightmode';
-    
-    // Set the base URL for your CSS files
-    const baseURL = '../css/'; // Adjust this if necessary to point to the correct folder
+    const baseURL = '/css/';
     document.getElementById('csstheme').setAttribute('href', `${baseURL}${csstheme}.css`);
 };
