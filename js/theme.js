aece6af0d71623c378714fb4deb4925a724d5c0a
MrTastyDuck <168537601+MrTastyDuck@users.noreply.github.com>
2025-01-07 23:16:14 +0100

    js fix change final help

--- js/theme.js
+++ js/theme.js
@@ -1,13 +1,20 @@
 function toggleCSSTheme() {
-    const currentCSSTheme = localStorage.getItem('csstheme') || 'lightmode';
-    const newCSSTheme = currentCSSTheme === 'lightmode' ? 'darkmode' : 'lightmode';
-    const baseURL = '/css/';
-    document.getElementById('csstheme').setAttribute('href', `${baseURL}${newCSSTheme}.css`);
-    localStorage.setItem('csstheme', newCSSTheme);
+  const currentCSSTheme = localStorage.getItem("csstheme") || "lightmode";
+  const newCSSTheme =
+    currentCSSTheme === "lightmode" ? "darkmode" : "lightmode";
+  const baseURL = "/css/";
+  document
+    .getElementById("csstheme")
+    .setAttribute("href", `${baseURL}${newCSSTheme}.css`);
+  localStorage.setItem("csstheme", newCSSTheme);
 }
-
-window.onload = function() {
-    const csstheme = localStorage.getItem('csstheme') || 'lightmode';
-    const baseURL = '/css/';
-    document.getElementById('csstheme').setAttribute('href', `${baseURL}${csstheme}.css`);
-};
+if (!localStorage.getItem("csstheme")) {
+  localStorage.setItem("csstheme", "lightmode");
+}
+if (localStorage.getItem("csstheme") == "darkmode") {
+  document.getElementById("csstheme").setAttribute("href", `/css/darkmode.css`);
+} else {
+  document
+    .getElementById("csstheme")
+    .setAttribute("href", `/css/lightmode.css`);
+}
